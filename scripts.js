f552887031264853afb604a485c4ffbf3aefe002
Guilherme Freschi <guilhermefreschix@gmail.com>
2024-11-28 12:52:57 -0300

    obtendo a moeda selecionada

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
+// Obtendo os elementos
+const form = document.querySelector('form')
 const amount = document.querySelector('#amount')
+const currency = document.getElementById('currency')
 
 // Manipulando o input amount para receber apenas números
 amount.addEventListener('input', () => {
   const hasCharacterRegex = /\D+/g
   amount.value = amount.value.replace(hasCharacterRegex, '')
 })
+
+// Captando o evento de submit do formulário
+form.onsubmit = (event) => {
+  event.preventDefault()
+  console.log(currency.value)
+}
